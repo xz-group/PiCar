b840872cb101bcc18934557f7a21eb8f4b4ac23f
jgu <jeffreygu.17@gmail.com>
2016-12-11 20:06:06 -0600

    twisted client rename

--- PiCar Program/twisted_client.py
+++ PiCar Program/twisted_client.py
@@ -1,3 +1,12 @@
+"""
+twisted_client.py
+
+AKA main_motive
+This script serves as the client-side program for receiving (solely) Motive positions from the MoCap PC. Results are outputted
+to a file with time interval benchmarking.
+
+"""
+
 import sys, socket, struct, time
 from twisted.internet.protocol import Protocol, Factory, ClientFactory
 from twisted.protocols.basic import LineOnlyReceiver
@@ -18,9 +27,7 @@ def findHost():
     addr = socket.gethostbyname(hostName)
     return addr
 
-
 class SocketClientProtocol(LineOnlyReceiver):
-    # after int prefix and other framing are removed:
     def lineReceived(self, line):
         self.factory.got_msg(line)
 
@@ -31,9 +38,6 @@ class SocketClientProtocol(LineOnlyReceiver):
 
 
 class SocketClientFactory(ClientFactory):
-    """ Created with callbacks for connection and receiving.
-        send_msg can be used to send messages when connected.
-    """
     protocol = SocketClientProtocol
 
     def __init__(self):
@@ -54,18 +58,12 @@ class SocketClientFactory(ClientFactory):
         self.client = client
 
     def got_msg(self, msg):
-        # data = literal_eval(msg)
         data = literal_eval(str(map(str,msg.split(','))))
 
         if self.dataBuff:
             self.dataBuff[0] = data
-
         else:
             self.dataBuff.append(data)
-
-        # for payload in self.dataBuff:
-        #     outputFileMotive.write(payload)
-        # outputFileMotive.write('\tTime Interval: %f\tRunning Time: %f\n' %(timeInterval, accumTime))
 
 def motiveSample():
     timePre = time.time()
@@ -73,6 +71,9 @@ def motiveSample():
     length = len(buffer)
     if buffer is not None and length > 0:
         data = buffer[0]
+
+        # FrameID, milliseconds, seconds, minutes, hours, x, y, z
+        # members from 'data' not included in 'sample': qx, qy, qz, qw
         sample = [data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]]
         print ('sample is %s\n', sample)
         timePost = time.time()
