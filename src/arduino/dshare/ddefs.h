497113e19cfdaaed462227c31003bc422bdf5f88
Humberto Gonzalez <hgonzale@wustl.edu>
2017-05-16 12:56:29 -0500

    basic dshare functionality

--- src/arduino/dshare/ddefs.h
+++ src/arduino/dshare/ddefs.h
@@ -2,27 +2,38 @@
 #define DDEF_H_
 
 // length of the internal storage array
-#define ARR_LENGTH 32
+#define DSHARE_ARR_LENGTH 32
 
+/*************************
+* Error Codes            *
+*************************/
+#define DSHARE_OK        0
+#define DSHARE_INVALID   1
+#define DSHARE_NOUPDATE  2
+
+
+/*************************
+* Addresses              *
+*************************/
 // IMU
-#define IMU_ACCEL_X      0 // see figure in page 10 of LSM9DS1 datasheet
-#define IMU_ACCEL_Y      1
-#define IMU_ACCEL_Z      2
-#define IMU_GYRO_ZY      3
-#define IMU_GYRO_XZ      4
-#define IMU_GYRO_YX      5
-#define IMU_MAGNET_X     6
-#define IMU_MAGNET_Y     7
-#define IMU_MAGNET_Z     8
-#define IMU_TEMP         9
-#define IMU_FILTER_A     10 // low pass filter parameters
-#define IMU_FILTER_B     11
+#define IMU_ACCEL_X      (uint8_t) 0 // see figure in page 10 of LSM9DS1 datasheet
+#define IMU_ACCEL_Y      (uint8_t) 1
+#define IMU_ACCEL_Z      (uint8_t) 2
+#define IMU_GYRO_ZY      (uint8_t) 3
+#define IMU_GYRO_XZ      (uint8_t) 4
+#define IMU_GYRO_YX      (uint8_t) 5
+#define IMU_MAGNET_X     (uint8_t) 6
+#define IMU_MAGNET_Y     (uint8_t) 7
+#define IMU_MAGNET_Z     (uint8_t) 8
+#define IMU_TEMP         (uint8_t) 9
+#define IMU_FILTER_A     (uint8_t) 10 // low pass filter parameters
+#define IMU_FILTER_B     (uint8_t) 11
 
 // Control Params
-#define SPEED_PID_P      12
-#define SPEED_PID_I      13
-#define SPEED_PID_D      14
-#define SPEED_PID_DFILT  15
+#define SPEED_PID_P      (uint8_t) 12
+#define SPEED_PID_I      (uint8_t) 13
+#define SPEED_PID_D      (uint8_t) 14
+#define SPEED_PID_DFILT  (uint8_t) 15
 
 // etc...
 
---
+++ src/arduino/dshare/dshare.c
@@ -0,0 +1,29 @@
+#include "dshare.h"
+#include "ddefs.h"
+
+int16_t dataArr[ DSHARE_ARR_LENGTH ];
+
+uint8_t initDataShare()
+{
+  return DSHARE_OK;
+}
+
+uint8_t getData( const uint8_t address, int16_t *data )
+{
+  if( address >= DSHARE_ARR_LENGTH )
+    return DSHARE_INVALID;
+
+  *data = dataArr[ address ];
+
+  return DSHARE_OK;
+}
+
+uint8_t setData( const uint8_t address, int16_t data )
+{
+  if( address >= DSHARE_ARR_LENGTH )
+    return DSHARE_INVALID;
+
+  dataArr[ address ] = data;
+
+  return DSHARE_OK;
+}
