a189cf6f4296267cb052894c850409e812dca5f1
willluer <wluer@wustl.edu>
2017-05-16 20:55:33 +0000

    TTC Calculated - see plot in folder

--- src/pi/Optical Flow/lk_ttc.py
+++ src/pi/Optical Flow/lk_ttc.py
@@ -31,6 +31,7 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 from numpy.linalg import inv
 import math
+import matplotlib.pyplot as plt
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
@@ -41,6 +42,7 @@ rawCapture = PiRGBArray(camera, size=(224, 128))
 
 # allow the camera to warmup
 time.sleep(2)
+
 #camera.exposure_mode = 'off'
 lk_params = dict( winSize  = (23, 23),
                   maxLevel = 3,
@@ -50,31 +52,33 @@ feature_params = dict( maxCorners = 500,
                        qualityLevel = 0.3,
                        minDistance = 7,
                        blockSize = 7 )
+ttcAvg = np.arange(10)
+FILTER_COUNTS = 10
 
 class App:
-    def __init__(self, video_src):
+    def __init__(self):
         self.track_len = 10
         self.detect_interval = 5
         self.tracks = []
-        self.cam = video.create_capture(video_src)
         self.frame_idx = 0
-        self.time = 0
         self.prev_time = 0
-
+        self.time = 0
+        self.foe = np.matrix([0,0]).reshape(2,1)
+        self.data = list()
+        self.runCount = list()
+        self.inc = 0
+        
     def run(self):
         for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
             #Read from camera
             img = frame.array
             
-            #Convert to grayscale
-            frame_gray_old = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-            #Equalize Histogram
-            frame_gray = cv2.equalizeHist(frame_gray_old)
-
+            #To grayscale and equalize
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+            frame_gray = clahe.apply(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
             
             vis = img.copy()
-
+            
             if len(self.tracks) > 0:
                 img0, img1 = self.prev_gray, frame_gray
                 p0 = np.float32([tr[-1] for tr in self.tracks]).reshape(-1, 1, 2)
@@ -82,35 +86,83 @@ class App:
                 p0r, st, err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **lk_params)
                 self.time = time.time()
                 
-                d = abs(p0-p0r).reshape(-1, 2).max(-1)
+                vec = abs(p0-p0r).reshape(-1, 2)
+                d = vec.max(-1)
                 good = d < 1
                 
                 new_tracks = []
+
+                i = 0
+                ttcCount = 0
+                ttcSum = 0
                 
-                for tr, (x, y),(x2,y2), good_flag in zip(self.tracks, p1.reshape(-1, 2),p0r.reshape(-1,2), good):
+                for tr, (x, y), (u,v), (x2,y2), good_flag in zip(self.tracks, p1.reshape(-1, 2), vec, p0r.reshape(-1,2), good):
                     if not good_flag:
                         continue
                     tr.append((x, y))
-                    di = y
-                    dDot = math.sqrt((x2-x)*(x2-x)+(y2-y)*(y2-y))/(self.time-self.prev_time)
-                    ttc = di/dDot
+                    
+                    #Calculate matrices for FOE
+                    b0 = x*v-y*u
+                    if i == 0:
+                        A = np.matrix([[u,v]])
+                        b = np.matrix([[b0]])
+                        i = i + 1
+                    else:
+                        A = np.vstack((A,(u,v)))
+                        b = np.vstack((b,b0))
+
                     if len(tr) > self.track_len:
                         del tr[0]
+                        
                     new_tracks.append(tr)
-                    cv2.putText(vis,'%.2f' % ttc, (x,y), cv2.FONT_HERSHEY_SIMPLEX,.3,(0,255,0))
-                    #draw_str(vis, (x,y), '%.2f' % ttc)
                     #cv2.circle(vis, (x, y), 2, (0, 255, 0), -1)
+
+                    #TTC
+                    d = math.sqrt((self.foe[0]-x)*(self.foe[0]-x)+(self.foe[1]-y)*(self.foe[1]-y))
+                    dDot = math.sqrt((x2-x)*(x2-x)+(y2-y)*(y2-y))/(self.time-self.prev_time)
+                    ttc = d/dDot
+
+                    #Print TTC
+                    if ttc < 15:
+                        cv2.putText(vis,'%.2f' % ttc, (x,y), cv2.FONT_HERSHEY_SIMPLEX,.3,(0,255,0))
+                        ttcCount = ttcCount+1
+                        ttcSum = ttc + ttcSum
+                        
+                #ROLLING AVERAGE FILTER
+                ttcTotalAvg = 0
+
+                if ttcCount > 0:
+                    self.inc = self.inc + 1        
+                    ttcAvg[self.inc % FILTER_COUNTS] = ttcSum/ttcCount
+                
+                    for val in ttcAvg:
+                        ttcTotalAvg = val + ttcTotalAvg
                     
+                    ttcTotalAvg = ttcTotalAvg/FILTER_COUNTS
+                
+                #Calculate new FOE
+                try:
+                        part1 = inv(np.matmul(A.transpose(),A))
+                except numpy.linalg.linalg.LinAlgErr as err:
+                    print('ERROROROROROR-------------------CAUGHT')
+                    self.foe = self.foe
+                else:
+                    self.foe = np.matmul(part1,np.matmul(A.transpose(),b))
+
+                if ttcTotalAvg != 0:
+                    self.data.append(ttcTotalAvg)
+                    self.runCount.append(len(self.data))
+                
                 self.tracks = new_tracks
                 #cv2.polylines(vis, [np.int32(tr) for tr in self.tracks], False, (0, 255, 0))
-                draw_str(vis, (20, 20), '%d' % len(self.tracks))
-
-                          
+                #draw_str(vis, (20, 20), '%d' % len(self.tracks))
+                draw_str(vis, (20, 20), '%.2f' % ttcTotalAvg)
+          
             if self.frame_idx % self.detect_interval == 0:
                 mask = np.zeros_like(frame_gray)
                 mask[:] = 255
-                #for x, y in [np.int32(tr[-1]) for tr in self.tracks]:
-                    #cv2.circle(mask, (x, y), 5, 0, -1)
+                for x, y in [np.int32(tr[-1]) for tr in self.tracks]:
+                    cv2.circle(mask, (x, y), 5, 0, -1)
                 p = cv2.goodFeaturesToTrack(frame_gray, mask = mask, **feature_params)
                 if p is not None:
                     for x, y in np.float32(p).reshape(-1, 2):
@@ -120,25 +172,27 @@ class App:
             self.frame_idx += 1
             self.prev_gray = frame_gray
             self.prev_time = self.time
+            
             cv2.imshow('lk_track', vis)
-            cv2.imshow('Before Equalization', frame_gray_old)
-            cv2.imshow('After Equalization',frame_gray)
+            #cv2.imshow('Before Equalization', frame_gray_old)
+            cv2.imshow('CLAHE (8,8)',frame_gray)
 
             ch = cv2.waitKey(1)
             rawCapture.truncate(0)
             
             if ch == 27:
+                plt.axis([0 , self.inc,0, 15])
+                plt.ylabel('Time to Contact (s)')
+                plt.xlabel('time')
+                plt.title('Time to Contact')
+                plt.plot(self.runCount,self.data)
+                plt.show()
                 break
 
 def main():
     import sys
-    try:
-        video_src = sys.argv[1]
-    except:
-        video_src = 0
-
     print(__doc__)
-    App(video_src).run()
+    App().run()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
