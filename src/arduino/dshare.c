0ab92ca0df43728d62438a15e7c6e04155f10636
Matt Kollada <mattkollada@gmail.com>
2017-05-17 17:01:54 -0500

    Moved dshare.c file

---
+++ src/arduino/dshare.c
@@ -0,0 +1,31 @@
+//#include "dshare.h"
+#include "ddefs.h"
+#include "stdint.h"
+
+int16_t dataArr[ DSHARE_ARR_LENGTH ];
+
+uint8_t initDataShare()
+{
+  return DSHARE_OK;
+}
+
+uint8_t getData( const uint8_t address, int16_t *data )
+{
+  if( address >= DSHARE_ARR_LENGTH )
+    return DSHARE_INVALID;
+
+  *data = dataArr[ address ];
+
+  return DSHARE_OK;
+}
+
+uint8_t setData( const uint8_t address, int16_t data )
+{
+  if( address >= DSHARE_ARR_LENGTH )
+    return DSHARE_INVALID;
+
+  dataArr[ address ] = data;
+
+  return DSHARE_OK;
+}
+
