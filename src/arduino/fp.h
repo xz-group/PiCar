0cf1e3feecfc94a7a045b2ac2fc5e2f858301be5
Humberto Gonzalez <hgonzale@wustl.edu>
2017-05-18 12:54:30 -0500

    added fp to other datatypes macros

--- src/arduino/fp.h
+++ src/arduino/fp.h
@@ -11,4 +11,13 @@ typedef _sAccum fp_t;
 #define fpmul(a,b) smulsk(a,b)
 #define fpdiv(a,b) sdivsk(a,b)
 
+// #define itofp(i) itok(i)
+// #define fptoi(k) ktoi(k)
+// #define ftofp(f) ftok(f)
+// #define fptof(k) ktod(k)
+#define itofp(i) itosk(i)
+#define fptoi(k) sktoi(k)
+#define ftofp(f) ftosk(f)
+#define fptof(k) sktof(k)
+
 #endif
