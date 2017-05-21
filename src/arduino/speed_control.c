27ba2571fa0a5e36851eb2375a6820823fa74684
Adith Jagadish Boloor <adith94@gmail.com>
2017-05-21 16:11:52 -0500

    Simple 'P'ID test

--- src/arduino/speed_control.c
+++ src/arduino/speed_control.c
@@ -9,26 +9,23 @@
 #include "speed_control.h"
 
 uint16_t PID = 1; // temporary constant
-uint16_t PWM_curr;
 uint16_t RPM_curr;
-uint16_t PWM_err;
+uint16_t PWM_new;
 uint16_t RPM_err;
 
 uint16_t toPWM(uint16_t RPM_des)
 {
-  if ( getData( BLDC_DUTY_CYCLE, &PWM_curr ) == DSHARE_OK );
   if ( getData( BLDC_HALL_SPEED, &RPM_curr ) == DSHARE_OK );
   
   RPM_err = RPM_des - RPM_curr;
-  PWM_err = PID*RPM_err;
-  PWM_curr += PWM_err;
+  PWM_new = PID*RPM_err;
 
   // Saturation between 0 and 255
-  if(PWM_curr > 255)
-    PWM_curr = 255;
-  else if(PWM_curr < 0)
-    PWM_curr = 0;
+  if(PWM_new > 255)
+    PWM_new = 255;
+  else if(PWM_new < 0)
+    PWM_new = 0;
     
-  return PWM_curr;
+  return PWM_new;
 }
 
