e18f803f353d4a414764a873eef050effde40b80
Kristen <kkoyanagi@wustl.edu>
2017-05-22 23:23:02 +0000

    python spi from pi side

--- src/pi/pythonSPI/RPI_Python_SPI.py
+++ src/pi/pythonSPI/RPI_Python_SPI.py
@@ -8,13 +8,15 @@ CS = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(CS,GPIO.OUT)
 
-SEND_PWM = -1
-SEND_SERVO = -2
+SEND_PWM = [1]
+SEND_SERVO = [2]
 
 spi = spidev.SpiDev()
 spi.open(0,0)
 to_send = [-1]
+something = [-1]
 pwm = [30]
+servo = [90]
 
 def sendCommand(one,two):
     try:
@@ -30,13 +32,27 @@ def sendCommand(one,two):
 
 def sendPWM():
     GPIO.output(CS,GPIO.LOW)
-    val1 = spi.xfer(to_send);
+    val1 = spi.xfer(SEND_PWM)
     val2 = spi.xfer(pwm)
     print(val1)
     print(val2)
+    GPIO.output(CS,GPIO.HIGH)
+
+def sendServoAngle():
+    GPIO.output(CS,GPIO.LOW);
+    val1 = spi.xfer(SEND_SERVO)
+    val2 = spi.xfer(servo)
+    print(val1)
+    print(val2)
+    GPIO.output(CS,GPIO.HIGH)
 
 while True:
-    sendPWM();
+    sendPWM()
+    time.sleep(1)
+    sendServoAngle()
+    time.sleep(1)
+
+
     
 
 
