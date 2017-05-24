8fca42cc4a2877b602f770332aa4d5d0cbb64ada
Kristen <kkoyanagi@wustl.edu>
2017-05-23 22:50:18 -0500

    primitive closed loop added

--- src/pi/Optical Flow/lk_ttc_constantFOEcloselooptest.py
+++ src/pi/Optical Flow/lk_ttc_constantFOEcloselooptest.py
@@ -21,6 +21,7 @@ ESC - exit
 from __future__ import print_function
 import time
 import numpy as np
+import spidev
 import cv2
 from common import anorm2, draw_str
 from time import clock
@@ -31,9 +32,23 @@ from picamera import PiCamera
 from numpy.linalg import inv
 import math
 ##import matplotlib.pyplot as plt
+import RPi.GPIO as GPIO
+import sys
+
+CS = 18
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(CS,GPIO.OUT)
+
+SEND_PWM = [1]
+SEND_SERVO = [2]
 
 width = 224
 height = 128
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (width, height)
@@ -44,9 +59,10 @@ rawCapture = PiRGBArray(camera, size=(width, height))
 # allow the camera to warmup
 time.sleep(2)
 
+#UNCOMMENT IF WANT TO RECORD VIDEO
 #initialize VideoWriter object
 fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-video1 = cv2.VideoWriter('constFOE.avi',fourcc, 20.0, (224,96))
+video1 = cv2.VideoWriter('constFOE.avi',fourcc, 20.0, (200,90))
 
 lk_params = dict( winSize  = (23, 23),
                   maxLevel = 3,
@@ -57,18 +73,36 @@ feature_params = dict( maxCorners = 500,
                        minDistance = 7,
                        blockSize = 7 )
 
-ttcAvg = np.arange(10)
 FILTER_COUNTS = 10
-ttcMin = 15
-class App:
+ttcAvg = np.arange(FILTER_COUNTS)
+ttcMin = 7
+
+def sendPWM(pwm):
+    GPIO.output(CS,GPIO.LOW)
+    val1 = spi.xfer(SEND_PWM)
+    val2 = spi.xfer(pwm)
+    print(val1)
+    print(val2)
+    GPIO.output(CS,GPIO.HIGH)
+
+def sendServoAngle(servo):
+    GPIO.output(CS,GPIO.LOW);
+    val1 = spi.xfer(SEND_SERVO)
+    val2 = spi.xfer(servo)
+    print(val1)
+    print(val2)
+    GPIO.output(CS,GPIO.HIGH)
+
+class App:  
+
     def __init__(self):
         self.track_len = 10
-        self.detect_interval = 5
+        self.detect_interval = 1
         self.tracks = []
         self.frame_idx = 0
         self.prev_time = 0
         self.time = 0
-        self.foe = np.matrix([112,48]).reshape(2,1)
+        self.foe = np.matrix([100,45]).reshape(2,1)
         self.data = list()
         self.runCount = list()
         self.inc = 0
@@ -78,10 +112,11 @@ class App:
             #------------------------READ AND PREPROCESS IMAGE-------------------------------------------------
             #Read from camera
             img = frame.array
-
+            
+            
             #Slice unnecessary pixels off image
-            img = img[int(height/8):int(7*height/8),:,:]
-            print(img.shape)
+            img = img[int(2*height/8):int(2*height/8+90),12:212,:]
+            
             #To grayscale and equalize
             clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
             frame_gray = clahe.apply(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
@@ -94,9 +129,11 @@ class App:
                 img0, img1 = self.prev_gray, frame_gray
                 
                 #puts in right data type and size
+                #p0 features in old frame to look for in new frame
                 p0 = np.float32([tr[-1] for tr in self.tracks]).reshape(-1, 1, 2)
                 
                 #calculates optical flow (second optical flow used for a validity check)
+                #p0 old coordinates, p1 new coordinates of same pixel
                 p1, st, err = cv2.calcOpticalFlowPyrLK(img0, img1, p0, None, **lk_params)
                 p0r, st, err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **lk_params)
 
@@ -104,6 +141,7 @@ class App:
                 self.time = time.time()
 
                 #Current points - previous points creates vector of displacement
+                #used in ttc calculation
                 vec = p1-p0
 
                 #Compare previous points with those found in second optical flow calculation
@@ -129,8 +167,6 @@ class App:
                     #add feature to track
                     tr.append((x, y))
                     
-                    #Calculate matrices for FOE (see paper for calculations)
-
                     if len(tr) > self.track_len:
                         del tr[0]
                         
@@ -142,17 +178,18 @@ class App:
                     #TTC (magnitude of displacement)/(velocity of displacement)
                     d = math.sqrt((self.foe[0]-x)*(self.foe[0]-x)+(self.foe[1]-y)*(self.foe[1]-y))
                     dDot = math.sqrt(u*u+v*v)/(self.time-self.prev_time)
+                    #u = (p1x - p0x)
+                    #v = (p1y - p0y)
 
                     #If dDot == 0, throws error so check
                     if dDot != 0:
                         ttc = d/dDot
                     else:
-                        ttc = 100
+                        continue
 
                     #Print TTC for relevant points (ttcMin instantiated at beginning of program)
                     if ttc < ttcMin:
                         cv2.putText(vis,'%.2f' % ttc, (x,y), cv2.FONT_HERSHEY_SIMPLEX,.3,(0,255,0))
-                        
                         ttcCount = ttcCount+1
                         ttcSum = ttc + ttcSum
                         xSum = xSum + x
@@ -178,25 +215,51 @@ class App:
                 #-----------------------------A PRETEND CONTROL OUTPUT---------------------------------------
                 #Decides which direction to turn based on location of the center of each point
                 #Tells how quickly to turn based on magnitude of the ttc average
+                temp = 90
                 if xAvg == 0:
                     #draw_str(vis, (20, 20), 'STRAIGHT')
                     cv2.putText(vis,'STRAIGHT', (5,80), cv2.FONT_HERSHEY_SIMPLEX,.3,(0,255,0))
                 elif xAvg < 112:
                     #draw_str(vis, (20, 20), 'RIGHT')
                     cv2.putText(vis,'RIGHT', (5,80), cv2.FONT_HERSHEY_SIMPLEX,.3,(0,255,0))
+                    temp = int((xAvg/2 + 90))
                 else:
                     #draw_str(vis, (20, 20), 'LEFT')
                     cv2.putText(vis,'LEFT', (5,80), cv2.FONT_HERSHEY_SIMPLEX,.3,(0,255,0))
-                    
+                    temp = int((90 + (xAvg-224)/2))
+
+                if temp > 130: 
+                    temp = 130
+                elif temp < 50:
+                    temp = 50
+
+                angle = [temp]
+
+##                if time.clock() > 2:
+##                    pwm = [int(ttcTotalAvg*255/5)]
+##                else:
+##                    pwm = 0
+##                    print("not yet")
+                
+   
                 if ttcTotalAvg == 0:
                     cv2.putText(vis,'NOTHING', (5,60), cv2.FONT_HERSHEY_SIMPLEX,.3,(0,255,0))
                     #draw_str(vis, (20, 20), 'NOTHING')
-                elif ttcTotalAvg < 7:
-                    cv2.putText(vis,'MOVE QUICKLY', (5,60), cv2.FONT_HERSHEY_SIMPLEX,.3,(0,255,0))
+                    pwm = [17]
+                elif ttcTotalAvg < 3:
+                    cv2.putText(vis,'MOVE NORMALLY', (5,60), cv2.FONT_HERSHEY_SIMPLEX,.3,(0,255,0))
                     #draw_str(vis, (20, 20), 'MOVE BITCH')
+                    pwm = [15]
                 else:
-                    cv2.putText(vis,'MOVE SLIGHTLY', (5,60), cv2.FONT_HERSHEY_SIMPLEX,.3,(0,255,0))
+                    cv2.putText(vis,'MOVE QUICKLY', (5,60), cv2.FONT_HERSHEY_SIMPLEX,.3,(0,255,0))
                     #draw_str(vis, (20, 20), 'MOVE SLIGHTLY')
+                    pwm = [16]
+
+                if time.clock() < 2:
+                    pwm = [0]
+
+                sendPWM(pwm)
+                sendServoAngle(angle)
                     
 
                 #Draw the FOE (not necessary)
@@ -217,10 +280,14 @@ class App:
             if self.frame_idx % self.detect_interval == 0:
                 mask = np.zeros_like(frame_gray)
                 mask[:] = 255
+                
                 for x, y in [np.int32(tr[-1]) for tr in self.tracks]:
                     cv2.circle(mask, (x, y), 5, 0, -1)
+                    
                 #get new features
+                cv2.imshow('mask',mask)
                 p = cv2.goodFeaturesToTrack(frame_gray, mask = mask, **feature_params)
+                
                 if p is not None:
                     for x, y in np.float32(p).reshape(-1, 2):
                         #add features to tracked points
@@ -228,14 +295,15 @@ class App:
 
 
             self.frame_idx += 1
+            
             self.prev_gray = frame_gray
             self.prev_time = self.time
 
             #Display the video
             cv2.imshow('lk_track', vis)
-            #cv2.imshow('Before Equalization', frame_gray_old)
-            #cv2.imshow('CLAHE (8,8)',frame_gray)
+            cv2.imshow('CLAHE (8,8)',frame_gray)
 
+            #UNCOMMENT TO WRITE FRAME TO VIDEO
             video1.write(vis)
             video1.write(vis)
             video1.write(vis)
@@ -247,7 +315,7 @@ class App:
 
             #plot when escape key is called
             if ch == 27:
-                video1.release()
+                #video1.release()
                 plt.axis([0 , self.inc,0, 15])
                 plt.ylabel('Time to Contact (s)')
                 plt.xlabel('time')
