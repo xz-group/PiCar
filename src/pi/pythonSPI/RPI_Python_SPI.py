255c93555d44c0d42a8cee7cac29bce03f52263d
Kristen <kkoyanagi@wustl.edu>
2017-05-28 17:32:32 +0000

    Matt's changes

--- src/pi/pythonSPI/RPI_Python_SPI.py
+++ src/pi/pythonSPI/RPI_Python_SPI.py
@@ -1,9 +1,9 @@
 import spidev
 import RPi.GPIO as GPIO
-import time
+from time import sleep
 import sys
 
-CS = 18
+CS = 22
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(CS,GPIO.OUT)
@@ -12,45 +12,36 @@ SEND_PWM = [1]
 SEND_SERVO = [2]
 
 spi = spidev.SpiDev()
-##spi.open(0,0)
+spi.open(0,0)
 to_send = [-1]
 something = [-1]
 pwm = [30]
 servo = [90]
-
-def sendCommand(one,two):
-    try:
-        GPIO.output(CS,GPIO.LOW)
-        val1 = spi.xfer(one)
-        val2 = spi.xfer(two)
-        GPIO.output(CS,GPIO.HIGH)
-        print(val1)
-        print(val2) 
-    except:
-        GPIO.cleanup()
-        sys.exit(0)
+DELAY = .01
 
 def sendPWM():
     GPIO.output(CS,GPIO.LOW)
     val1 = spi.xfer(SEND_PWM)
     val2 = spi.xfer(pwm)
+    sleep(DELAY)
+    GPIO.output(CS,GPIO.HIGH)
     print(val1)
     print(val2)
-    GPIO.output(CS,GPIO.HIGH)
 
 def sendServoAngle():
     GPIO.output(CS,GPIO.LOW);
     val1 = spi.xfer(SEND_SERVO)
     val2 = spi.xfer(servo)
+    sleep(DELAY)
+    GPIO.output(CS,GPIO.HIGH)
     print(val1)
     print(val2)
-    GPIO.output(CS,GPIO.HIGH)
 
 while True:
     sendPWM()
-    time.sleep(1)
+    sleep(1)
     sendServoAngle()
-    time.sleep(1)
+    sleep(1)
 
 
     
